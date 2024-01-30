c9ac33676352d91f7bfa1c4c076333ed25e56dcd
DESKTOP-SQUM9RL\dev <rosemary@modiila.com>
2024-01-30 15:46:45 +0900

    html

--- js/common.js
+++ js/common.js
@@ -1,3 +1,13 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
+
 document.addEventListener("DOMContentLoaded", function() {
 
   const header = document.getElementById('header');
