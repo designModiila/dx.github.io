49ed601ebce948bc37e035a7e334bef923b38586
DESKTOP-SQUM9RL\dev <rosemary@modiila.com>
2024-02-16 18:37:22 +0900

    수정

--- js/common.js
+++ js/common.js
@@ -354,12 +354,12 @@ $(document).ready(function() {
 
 
 
-$(window).mousemove(function (e) {
-	$(".ring").css(
-		"transform",
-		`translateX(calc(${e.clientX}px - 1.25rem)) translateY(calc(${e.clientY}px - 1.25rem))`
-	);
-});
+// $(window).mousemove(function (e) {
+// 	$(".ring").css(
+// 		"transform",
+// 		`translateX(calc(${e.clientX}px - 1.25rem)) translateY(calc(${e.clientY}px - 1.25rem))`
+// 	);
+// });
 
 (function ($) {
 	"use strict";
